c7b1eef6684087a37b7cfbf0f79d13024ca356b1
Azusa Mori <1911140069x@ed.fuk.kindai.ac.jp>
2021-10-18 08:50:50 +0000

    validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  #TODO: validates
+  validates :uid, uniqueness: true
   has_many :tweets
   has_many :likes
   has_many :like_tweets, through: :likes, source: :tweet
